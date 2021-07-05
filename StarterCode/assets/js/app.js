f538918e2805fc064addb38c682e65e84bd0fc98
ElVejigante <ernesto.cancel@gmail.com>
2021-07-05 19:20:09 -0400

    Update app.js

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -18,3 +18,6 @@ let chart = d3.select('#scatter').append('div').classed('chart', true);
 
 // append svg to chart:
 let svg = chart.append('svg').attr('width', svgWidth).attr('height', svgHeight);
+
+//append svg group:
+let svgGroup = svg.append('g').attr('transform',`translate(${margin.left}, ${margin.top})`);
